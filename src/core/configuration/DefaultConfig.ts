7fb180e7dce12c16bb3240c79e37cd50a819e53c
1brucben <1benjbruce@gmail.com>
2025-04-26 01:41:45 +0200

    bot initial pop nerf

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -534,7 +534,7 @@ export class DefaultConfig implements Config {
 
   startManpower(playerInfo: PlayerInfo): number {
     if (playerInfo.playerType == PlayerType.Bot) {
-      return 8_000;
+      return 6_000;
     }
     if (playerInfo.playerType == PlayerType.FakeHuman) {
       switch (this._gameConfig.difficulty) {

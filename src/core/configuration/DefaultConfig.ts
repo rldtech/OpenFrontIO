86415dafe9d3f323de298ff6402d783129c152b1
1brucben <1benjbruce@gmail.com>
2025-04-26 03:10:17 +0200

    less gold in game

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -639,7 +639,7 @@ export class DefaultConfig implements Config {
     const cityGold = cityWorkers / 2000;
     const tileGold = (ruralWorkers ** 0.5 * numTiles ** 0.5) / 600;
 
-    const totalGold = 13 * (cityGold + tileGold) ** 0.8;
+    const totalGold = 10 * (cityGold + tileGold) ** 0.8;
 
     return Number.isFinite(totalGold) ? totalGold : 0;
   }

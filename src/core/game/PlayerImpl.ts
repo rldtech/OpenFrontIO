0811b23e00ef32caf03cfdde2fc2c9e49073d7cc
evan <openfrontio@gmail.com>
2025-05-06 09:52:13 -0700

    teammode: bots can attack each other now to prevent them from ganging up on players

--- src/core/game/PlayerImpl.ts
+++ src/core/game/PlayerImpl.ts
@@ -606,6 +606,9 @@ export class PlayerImpl implements Player {
     if (this.team() == null || other.team() == null) {
       return false;
     }
+    if (this.team() == Team.Bot || other.team() == Team.Bot) {
+      return false;
+    }
     return this._team == other.team();
   }
 

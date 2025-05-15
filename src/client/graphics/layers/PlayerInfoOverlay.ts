5da577d8796bf97ab37f5d8bea905408c5e8944c
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-05-14 22:14:53 -0400

    src/client/graphics/layers/PlayerInfoOverlay.ts

--- src/client/graphics/layers/PlayerInfoOverlay.ts
+++ src/client/graphics/layers/PlayerInfoOverlay.ts
@@ -207,7 +207,7 @@ export class PlayerInfoOverlay extends LitElement implements Layer {
             : ""}
           ${player.name()}
         </div>
-        ${player.team() != null
+        ${player.team() !== null
           ? html`<div class="text-sm opacity-80">Team: ${player.team()}</div>`
           : ""}
         <div class="text-sm opacity-80">Type: ${playerType}</div>

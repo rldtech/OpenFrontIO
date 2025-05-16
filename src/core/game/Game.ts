03cef7ff069177fa79334592599912f589c8c7ff
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-05-16 16:42:29 -0400

    src/core/game/Game.ts

--- src/core/game/Game.ts
+++ src/core/game/Game.ts
@@ -198,7 +198,7 @@ export const nukeTypes = [
   UnitType.HydrogenBomb,
   UnitType.MIRVWarhead,
   UnitType.MIRV,
-] as UnitType[];
+] as const satisfies UnitType[];
 
 export type NukeType = (typeof nukeTypes)[number];
 
@@ -513,7 +513,7 @@ export interface Player {
 }
 
 export interface Game extends GameMap {
-  expireAlliance(alliance: Alliance);
+  expireAlliance(alliance: Alliance): void;
   // Map & Dimensions
   isOnMap(cell: Cell): boolean;
   width(): number;

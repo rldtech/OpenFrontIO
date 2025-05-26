d105d21d2035c9cd234aad676aa43ae4975d9f19
icslucas <carolinacarazolli@gmail.com>
2025-05-26 20:22:23 +0200

    fix

--- src/client/graphics/layers/UnitLayer.ts
+++ src/client/graphics/layers/UnitLayer.ts
@@ -403,7 +403,7 @@ export class UnitLayer implements Layer {
           case UnitType.AtomBomb:
             this.soundManager.playSound("atomlaunch");
           case UnitType.HydrogenBomb:
-            this.soundManager.playSound("hydolaunch");
+            this.soundManager.playSound("hyrdolaunch");
             break;
         }
       }
